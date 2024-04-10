8060b47ebe7d5087925e5653d4e947da3794516e
kyungil.david <kyungil.david@navercorp.com>
2024-04-10 16:36:33 +0900

    Set initial code from antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {InboxOutlined} from '@ant-design/icons';
+import type {UploadProps} from 'antd';
+import {message, Upload} from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+const {Dragger} = Upload;
+
+const props: UploadProps = {
+    name: 'file',
+    multiple: false,
+    accept: '.xlsx',
+    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
+    onChange(info) {
+        const {status} = info.file;
+        if (status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+    onDrop(e) {
+        console.log('Dropped files', e.dataTransfer.files);
+    },
+};
+
+const App: React.FC = () => (
+    <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+            <InboxOutlined/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+            banned files.
+        </p>
+    </Dragger>
+);
 
 export default App;
